45d936530c80352c44b8c95e0d0387d85b03d765
MarcoDDM <marantal2013@hotmail.com>
2021-01-26 19:56:47 -0300

    first

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter, Route, Switch} from 'react-router-dom';
+import Home from './pages/Home.js'; 
+import Upload from './pages/Upload.js';
+
 import './index.css';
-import App from './App';
+
+const App = () => {
+  return (
+    <HashRouter>
+      <Switch>
+        <Route path='/upload' component={Upload}/>
+        <Route path='/' component={Home}/>
+      </Switch>
+    </HashRouter>
+  );
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
